901125e7b878b33720da06890bc63588efef70dd
Pablo <prodriguezrial@gmail.com>
2026-06-06 22:51:28 +0100

    fix: add clerk proxy rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/__clerk/:path*',
+        destination: 'https://clerk.traqcker.com/__clerk/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
